6daadcef793e5db685a0b21f3d9fcb2724c074dd
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-01-30 13:10:21 +0100

    2

---
+++ main..js
@@ -0,0 +1,13 @@
+// 4 4l4m4nty globalne
+
+let size = 10;
+let orderElement = 1;
+
+const init = () => {
+  const btn = document.createElement("button"); // TWORZYMY STRUKTURE BUTTON
+  btn.textContent = ""; // ROBIMY OPIS NA BUTTON
+  btn.textContent = "Dodaj elementy"; // ROBIMY OPIS NA BUTTON
+  document.body.appendChild(btn); // wskazujemy gdzie dodać przycisk,za pomocą metody appendChild dodajemy przycisk tak żeby był widoczny na stronie
+};
+
+const createLiElement = () => {};

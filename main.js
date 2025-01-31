8c8249df376438457cf7f970da18cbccb95a15a9
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-01-31 08:18:38 +0100

    last commit

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
-  const btn = document.createElement("button"); // TWORZYMY STRUKTURE BUTTON
+  const btn = document.createElement("button");
+  const btnReset = document.createElement("button"); // TWORZYMY STRUKTURE BUTTON
 
   btn.textContent = "Dodaj elementy"; // ROBIMY OPIS NA BUTTON
+  btnReset.textContent = "RESET";
   document.body.appendChild(btn); // wskazujemy gdzie dodać przycisk,za pomocą metody appendChild dodajemy przycisk tak żeby był widoczny na stronie
+  document.body.appendChild(btnReset); // analogicznie jw
 
   const ul = document.createElement("ul"); //analogicznie jak powyżej tworzymy ul  i dodajemy do body
   document.body.appendChild(ul);
   //dodajemy nasłuchiwanie na elemncie button, w nawiasach apisujemy czego ma nasłuchiwac i jaką funkcje uruchamiać
   btn.addEventListener("click", createLiElement);
+  btnReset.addEventListener("click", cleanList);
 };
 
 //tworzymy elementy li za pomocą pętli, tworzymy tyle ile jest wyznaczone w iteracji
@@ -20,8 +24,15 @@ const createLiElement = () => {
   for (let i = 0; i < 10; i++) {
     const li = document.createElement("li");
     li.textContent = `Element nr${orderElement++}`; //podpisujemy elementy
+    li.style.fontSize = `${size++}px`; //z każdą iteracją zwiększamy rozmiar czcionki
     document.querySelector(`ul`).appendChild(li); //pobieramy listę ul w której będą się tworzyć li, i dodajemy li do środka
   }
+};
+
+const cleanList = () => {
+  document.querySelector(`ul`).textContent = "";
+  size = 10; //po resecie przywracamy do pierwotnych wartości
+  orderElement = 1;
 };
 
 init();
